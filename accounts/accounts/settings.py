3ee029c6cbc851cdf0e9c80bf72b10f4934d90c6
Shijar Muhammed K <115169317+shijar-muhammed-k@users.noreply.github.com>
2023-07-23 21:33:51 +0530

    host

--- accounts/accounts/settings.py
+++ accounts/accounts/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a-#b604@v_sga_a2r-^nc94vrw&i!ixqmk($vvbuso_6&+4*qr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
