7fe993ebd9fe42544d5690f6edd7316c3c707392
Shijar Muhammed K <115169317+shijar-muhammed-k@users.noreply.github.com>
2023-07-23 21:33:10 +0530

    db

--- accounts/accounts/settings.py
+++ accounts/accounts/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'accounts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shopizfinalTesti$default',
+        'USER': 'shopizfinalTesti',
+        'PASSWORD': 'Shijar@2000',
+        'HOST':'shopizfinalTesting.mysql.pythonanywhere-services.com',
+        'PORT':'3306',
     }
 }
 
